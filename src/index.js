a87cdcd14d4ae0f5a2710d6ffba4bfa14208fa21
Net-zen <prankster@myttk.ru>
2019-09-29 19:18:52 +0600

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+  const bracketsStack = [];
+  let res = true;
+  let stop = false;
+    for (let i = 0; i < str.length; i++) {
+      if (!stop){
+          bracketsConfig.forEach(element => {
+              if (str[i] === element[0]){
+                if (str[i] === element[1] && str[i] === bracketsStack[bracketsStack.length - 1] ){
+                  bracketsStack.pop();
+                } else {
+                  bracketsStack.push(str[i]);
+                }
+              } else if (str[i] === element[1]){
+                  if (bracketsStack.length === 0){
+                    stop = true;
+                    res = false;
+                    return false;
+                  } else  if (bracketsStack[bracketsStack.length - 1] === element[0]){
+                    bracketsStack.pop();
+                    res = true;
+                  }
+              } 
+          });
+      }
+    } 
+    (bracketsStack.length !== 0 || stop) ? res = false : res = true;
+    return res;
 }
+
+
